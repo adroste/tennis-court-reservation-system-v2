ebea9d4c6b5a2f88bc77d564d36c625a32839068
Alexander Droste <mail@alexanderdroste.de>
2020-11-01 16:34:59 +0100

    calendar autoscroll to selected date

--- client/src/calendar/ReservationCalendar.js
+++ client/src/calendar/ReservationCalendar.js
@@ -1,17 +1,21 @@
+import React, { useEffect, useRef } from 'react';
+
 import { DayTable } from './DayTable';
 import { HoursTable } from './HoursTable';
-import React from 'react';
 import { WeekPicker } from './WeekPicker';
 import dayjs from 'dayjs';
 import styles from './ReservationCalendar.module.css';
 
 export function ReservationCalendar() {
 
+    const scrollerRef = useRef();
+
     const reservations = [
         ['2020-10-24', 'Platz 1', 13, 'Müller'],
         ['2020-10-24', 'Platz 2', 13, 'Peter Jacob'],
         ['2020-10-25', 'Platz 1', 13, 'Müller Gustagv Petersen'],
         ['2020-10-20', 'Platz 1', 8, 'Müller Gustagv Petersen'],
+        ['2020-10-20', 'Platz 1', 10, 'Müller Gustagv Petersen franz dieter franx'],
         ['2020-10-20', 'Platz 1', 18, 'Vera Berger'],
         ['2020-10-20', 'Platz 2', 18, 'Christian15lww'],
     ];
@@ -27,11 +31,18 @@ export function ReservationCalendar() {
         visibleHours.push(i);
 
     const visibleDatesCount = 7;
+    const focusedDateIndex = 0;
     const selectedDate = dayjs('2020-10-19');
     const visibleDates = [];
     for (let i = 0; i < visibleDatesCount; ++i) {
         visibleDates.push(selectedDate.add(i, 'day'));
     }
+
+    useEffect(() => {
+        if (focusedDateIndex)
+            scrollerRef.current.scrollLeft 
+                = ((scrollerRef.current.scrollWidth) / visibleDatesCount) * focusedDateIndex;
+    }, [focusedDateIndex, visibleDatesCount]);
 
     return (
         <div className={styles.wrapper}>
@@ -42,7 +53,7 @@ export function ReservationCalendar() {
             <div className={styles.tableWrapper}>
                 <HoursTable visibleHours={visibleHours} />
 
-                <div className={styles.tableScroller}>
+                <div className={styles.tableScroller} ref={scrollerRef}>
                     {visibleDates.map(date => (
                         <DayTable
                             key={date}
