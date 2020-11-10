8ea624ebd40c267a795d211fe487a3c192b6399a
Alexander Droste <mail@alexanderdroste.de>
2020-11-10 02:33:29 +0100

    Login Page

---
+++ client/src/user/LoginPage.js
@@ -0,0 +1,67 @@
+import { Button, Checkbox, Divider, Form, Input, Typography } from 'antd';
+import { LockOutlined, MailOutlined, UserOutlined } from '@ant-design/icons';
+import React, { useContext, useEffect } from 'react';
+
+import styles from './LoginPage.module.css';
+
+export function LoginPage() {
+
+    const onFinish = values => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <div className={styles.wrapper}>
+            <Typography.Title level={3}>Anmelden</Typography.Title>
+
+            <Form
+                layout="vertical"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+            >
+                <Form.Item
+                    name="email"
+                    rules={[{ required: true, message: 'E-Mail Adresse ist erforderlich' }]}
+                >
+                    <Input prefix={<MailOutlined />} placeholder="E-Mail" />
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: 'Passwort ist erforderlich' }]}
+                >
+                    <Input.Password prefix={<LockOutlined />} placeholder="Passwort" />
+                </Form.Item>
+
+                <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                        <Checkbox>Angemeldet bleiben</Checkbox>
+                    </Form.Item>
+
+                    <a className={styles.forgotLink} href="">
+                        Password vergessen
+                    </a>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className={styles.loginButton}>
+                        Anmelden
+                    </Button>
+                </Form.Item>
+                
+
+                <Form.Item className={styles.registerItem}>
+                    <span>oder</span>
+
+                    <Button type="link">
+                        Kostenlos Registrieren
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+}
