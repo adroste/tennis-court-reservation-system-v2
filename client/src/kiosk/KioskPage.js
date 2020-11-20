b03b1b88adc34376730176b5235238f390ea69c9
Alexander Droste <mail@alexanderdroste.de>
2020-11-20 06:02:55 +0100

    KioskPage change update intervals (dynamic)

--- client/src/kiosk/KioskPage.js
+++ client/src/kiosk/KioskPage.js
@@ -1,11 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { UPDATE_INTERVALS_SEC, setDefaultUpdateIntervals } from '../updateIntervals';
 
 import { Alert } from 'antd';
 import { DateClock } from './DateClock';
 import { ReservationCalendar } from '../calendar/ReservationCalendar';
 import { SystemLinkQr } from './SystemLinkQr';
 import { appContext } from '../AppContext';
+import { parseQuery } from '../helper';
 import styles from './KioskPage.module.css';
+import { useLocation } from 'react-router-dom';
 import { useTime } from '../calendar/useTime';
 
 export function KioskPage() {
@@ -13,6 +16,20 @@ export function KioskPage() {
     const { config: { announcement, orgName }, courts } = useContext(appContext);
 
     const hour = useTime('hour');
+    const { search } = useLocation();
+
+    useEffect(() => {
+        const params = parseQuery(search);
+        let updateInterval = 60; // 1 minute, default value
+        try {
+            if (params['update'])
+                updateInterval = parseInt(params['update']);
+        } catch (e) { console.error(e); }
+
+        UPDATE_INTERVALS_SEC.RESERVATIONS = updateInterval;
+        UPDATE_INTERVALS_SEC.BASE_DATA = updateInterval * 10;
+        return setDefaultUpdateIntervals;
+    }, [search]);
 
     return (
         <div className={styles.wrapper}>
